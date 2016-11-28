1a1ebb16e51089da02970362c2754a245a52a394
D S Pavan Kumar <dspavankumar@gmail.com>
2016-11-29 01:18:39 +0530

    Added command line control

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # A Simple MFCC Feature Extractor using C++ STL and C++11
 ## Features
-* Takes PCM Wave input.
-* Outputs MFCCs as comma separated floating point values, each line representing a frame.
-* Applies pre-emphasis, Hamming window, computes FFT and power spectrum.
-* Applies equal height log-Mel filterbank followed by discrete cosine transform.
-* Window length, frame shift, sampling rate and number of cepstra are configurable in the wrapper script.
-* Outputs log-energy as the first coefficient, followed by the specified number of cepstra.
+* Takes PCM Wave input and outputs MFCCs as comma separated floating point values, each line representing a frame.
+* Supports batch extraction through list input and output.
+* Command line control for window length, frame shift, sampling rate, number of cepstra and filterbank cutoff frequencies.
+* Computes MFCCs through pre-emphasis, Hamming window, FFT, power spectrum, equal height log-Mel filterbank followed by discrete cosine transform.
 
 ## Compilation
 g++ -std=c++11 -O3 compute-mfcc.cc -o compute-mfcc
 
-## Usage
-./compute-mfcc input.wav output.mfc
+## Usage Examples
+compute-mfcc --input input.wav --output output.mfc
+compute-mfcc --input input.wav --output output.mfc --samplingrate 8000
+compute-mfcc --inputlist input.list --outputlist output.list
+compute-mfcc --inputlist input.list --outputlist output.list --numcepstra 17 --samplingrate 44100
 
 ## License
 GNU GPL V3.0
--- compute-mfcc.cc
+++ compute-mfcc.cc
@@ -18,46 +18,41 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include "mfcc.cc"
 
-int main(int argc, char* argv[]) {
-    // Check arguments
-    if (argc != 3) {
-        std::cout << "Usage: compute-mfcc input.wav output.mfc" << std::endl;
-        return 0;
-    }
-    
-    // Assign paths
-    const char *wavPath=argv[1], *mfcPath=argv[2];
+// A simple option parser
+char* getCmdOption(char **begin, char **end, const std::string &value) {
+    char **iter = std::find(begin, end, value);
+    if (iter != end && ++iter != end)
+        return *iter;
+    return nullptr;
+}
 
+// Process each file
+int processFile (MFCC &mfccComputer, const char* wavPath, const char* mfcPath) {
     // Initialise input and output streams    
     std::ifstream wavFp;
-    wavFp.open(wavPath);
     std::ofstream mfcFp;
-    mfcFp.open(mfcPath);
     
     // Check if input is readable
+    wavFp.open(wavPath);
     if (!wavFp.is_open()) {
         std::cerr << "Unable to open input file: " << wavPath << std::endl;
         return 1;
     }
+    
     // Check if output is writable
+    mfcFp.open(mfcPath);
     if (!mfcFp.is_open()) {
         std::cerr << "Unable to open output file: " << mfcPath << std::endl;
         wavFp.close();
         return 1;
     }
-
-    // Initialise MFCC class instance
-    int winLength=25, frameShift=10; // Both in milliseconds
-    int fs=16000;        // Sampling frequency in Hertz
-    int numCepstra = 12; // Number of cepstra, excluding log-energy
-    double lowFreq = 50.0, highFreq=6500; // Low and high cutoff frequencies for filterbank
-    MFCC mfccComputer (fs, numCepstra, winLength, frameShift, lowFreq, highFreq);
-    
-    // Process
+   
+    // Extract and write features
     if (mfccComputer.process (wavFp, mfcFp))
         std::cerr << "Error processing " << wavPath << std::endl;
 
@@ -65,3 +60,99 @@ int main(int argc, char* argv[]) {
     mfcFp.close();
     return 0;
 }
+
+// Process lists
+int processList (MFCC &mfccComputer, const char* wavListPath, const char* mfcListPath) {
+    std::ifstream wavListFp, mfcListFp;
+
+    // Check if wav list is readable
+    wavListFp.open(wavListPath);
+    if (!wavListFp.is_open()) {
+        std::cerr << "Unable to open input list: " << wavListPath << std::endl;
+        return 1;
+    }
+
+    // Check if mfc list is readable
+    mfcListFp.open(mfcListPath);
+    if (!mfcListFp.is_open()) {
+        std::cerr << "Unable to open output list: " << mfcListPath << std::endl;
+        return 1;
+    }
+
+    // Process lists
+    std::string wavPath, mfcPath;
+    while (true) {
+        std::getline (wavListFp, wavPath);
+        std::getline (mfcListFp, mfcPath);
+        if (wavPath.empty() || mfcPath.empty())
+            return 0;
+        if (processFile (mfccComputer, wavPath.c_str(), mfcPath.c_str()))
+            return 1;
+    }
+
+    return 0;
+}
+
+// Main
+int main(int argc, char* argv[]) {
+    std::string USAGE = "compute-mfcc : MFCC Extractor\n";
+    USAGE += "OPTIONS\n";
+    USAGE += "--input           : Input 16 bit PCM Wave file\n";
+    USAGE += "--output          : Output MFCC file in CSV format, each frame in a line\n";
+    USAGE += "--inputlist       : List of input Wave files\n";
+    USAGE += "--outputlist      : List of output MFCC CSV files\n";
+    USAGE += "--numcepstra      : Number of output cepstra, excluding log-energy (default=12)\n";
+    USAGE += "--samplingrate    : Sampling rate in Hertz (default=16000)\n";
+    USAGE += "--winlength       : Length of analysis window in milliseconds (default=25)\n";
+    USAGE += "--frameshift      : Frame shift in milliseconds (default=10)\n";
+    USAGE += "--lowfreq         : Filterbank low frequency cutoff in Hertz (default=50)\n";
+    USAGE += "--highfreq        : Filterbank high freqency cutoff in Hertz (default=samplingrate/2)\n";
+    USAGE += "USAGE EXAMPLES\n";
+    USAGE += "compute-mfcc --input input.wav --output output.mfc\n";
+    USAGE += "compute-mfcc --input input.wav --output output.mfc --samplingrate 8000\n";
+    USAGE += "compute-mfcc --inputlist input.list --outputlist output.list\n";
+    USAGE += "compute-mfcc --inputlist input.list --outputlist output.list --numcepstra 17 --samplingrate 44100\n";
+
+	char *wavPath = getCmdOption(argv, argv+argc, "--input");
+	char *mfcPath = getCmdOption(argv, argv+argc, "--output");
+	char *wavListPath = getCmdOption(argv, argv+argc, "--inputlist");
+	char *mfcListPath = getCmdOption(argv, argv+argc, "--outputlist");
+	char *numCepstraC = getCmdOption(argv, argv+argc, "--numcepstra");
+	char *samplingRateC = getCmdOption(argv, argv+argc, "--samplingrate");
+	char *winLengthC = getCmdOption(argv, argv+argc, "--winlength");
+	char *frameShiftC = getCmdOption(argv, argv+argc, "--frameshift");
+	char *lowFreqC = getCmdOption(argv, argv+argc, "--lowfreq");
+	char *highFreqC = getCmdOption(argv, argv+argc, "--highfreq");
+
+    // Check arguments
+    if ((argc<3) || (!(wavPath && mfcPath) && !(wavListPath && mfcListPath))) {
+        std::cout << USAGE;
+        return 1;
+    }
+    
+    // Assign variables
+    int numCepstra = (numCepstraC ? atoi(numCepstraC) : 12);
+    int samplingRate = (samplingRateC ? atoi(samplingRateC) : 16000);
+    int winLength = (winLengthC ? atoi(winLengthC) : 25);
+    int frameShift = (frameShiftC ? atoi(frameShiftC) : 10);
+    int lowFreq = (lowFreqC ? atoi(lowFreqC) : 50);
+    int highFreq = (highFreqC ? atoi(highFreqC) : samplingRate/2);
+
+    // Initialise MFCC class instance
+    MFCC mfccComputer (samplingRate, numCepstra, winLength, frameShift, lowFreq, highFreq);
+
+    // Process wav files
+    if (wavPath && mfcPath)
+        if (processFile (mfccComputer, wavPath, mfcPath))
+            return 1;
+
+    // Process lists
+    if (wavListPath && mfcListPath)
+        if (processList (mfccComputer, wavListPath, mfcListPath))
+            return 1;
+
+    return 0;
+}
+
+
+
